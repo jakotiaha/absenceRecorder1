aed2f5dc5fda9ea0a19ae10f514918cc5d5f17c1
jakotiaha <jakotiah.a@etoncollege.org.uk>
2023-01-29 21:22:28 +0000

    Initial Commit

---
+++ absenceRecorder1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  absenceRecorder1
+//
+//  Created by Akshat Jakotiah on 29/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ absenceRecorder1/absenceRecorder1App.swift
@@ -0,0 +1,17 @@
+//
+//  absenceRecorder1App.swift
+//  absenceRecorder1
+//
+//  Created by Akshat Jakotiah on 29/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct absenceRecorder1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
