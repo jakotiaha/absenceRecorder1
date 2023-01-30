9946d46da4bd8833cc1f99703eeefad0d53174cf
jakotiaha <jakotiah.a@etoncollege.org.uk>
2023-01-30 14:27:36 +0000

    adding basic model

---
+++ absenceRecorder1/model/division.swift
@@ -0,0 +1,19 @@
+//
+//  division.swift
+//  absenceRecorder1
+//
+//  Created by Akshat Jakotiah on 30/01/2023.
+//
+
+import Foundation
+
+class Division{
+    
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    
+    }
+}
